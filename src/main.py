0f9a7d45a4b0c3058f21f340e911b1a2c137aff5
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2020-01-27 14:56:52 +0000

    Added artificial datasets

--- src/main.py
+++ src/main.py
@@ -13,18 +13,18 @@ import pandas as pd
 
 NetworkMetadata = namedtuple('NetworkMetadata', 'name structure activation_function batch_size epochs id')
 network_info = NetworkMetadata(name='LetterRecognition',
-                               activation_function='tanh',
-                               structure=[16, 30, 28, 26, 26],
+                               activation_function='relu',
+                               structure=[16,30,28,26,26],
                                id=randint(0, 10000),
-                               batch_size=80,
-                               epochs=100)
+                               batch_size=50,
+                               epochs=500)
 datapath = 'preprocessing/%s/' % network_info.name
 
-train_data = pd.read_csv(datapath + 'train_data.csv')
+train_data = pd.read_csv(datapath + 'train_data_nonbin.csv')
 X_train = train_data.drop(['target'], axis=1).iloc[:, :].values
 y_train = train_data['target'].values
 
-test_data = pd.read_csv(datapath + 'test_data.csv')
+test_data = pd.read_csv(datapath + 'test_data_nonbin.csv')
 X_test = test_data.drop(['target'], axis=1).iloc[:, :].values
 y_test = test_data['target'].values
 
@@ -35,10 +35,10 @@ col_names = train_data.columns
 
 # ----------------------------------------- Cross Validation Step ----------------------------------------------------
 # Cross Validate with different parameters until reach best accuracy on validation data
-cross_validate(X=X_train,
-               y=y_train,
-               n_folds=5,
-               network_info=network_info)
+# cross_validate(X=X_train,
+#                y=y_train,
+#                n_folds=5,
+#                network_info=network_info)
 # --------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------- Final Model Training ---------------------------------------------------
@@ -49,8 +49,8 @@ model = build_network.build(structure=network_info.structure, activation_functio
 class_weights = dict(enumerate(class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)))
 
 # Train Model
-model.fit(X_train,  # training data inputs
-          to_categorical(y_train),  # training data targets
+model.fit(X_train,
+          to_categorical(y_train),
           batch_size=network_info.batch_size,
           epochs=network_info.epochs,
           verbose=0,
@@ -66,6 +66,7 @@ model.save('%s%s_%d.h5' % (filepath, network_info.name, network_info.id))
 network_structure = '-'.join(str(layer) for layer in network_info.structure)
 with open(filepath + 'information.txt', 'a') as file:
     file.write(str(network_info) + '\n')
+    file.write('n_train: %d n_test: %d \n' % (len(X_train), len(X_test)))
     file.write('Model Accuracy: ' + str(accuracy) + '\n\n')
 # --------------------------------------------------------------------------------------------------------------------
 
