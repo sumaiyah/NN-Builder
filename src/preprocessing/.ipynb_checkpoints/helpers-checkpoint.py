b701de3a791e9bd03ccc06947651878274e6fc32
Sumaiyah Kola <sumaiyahkola1@gmail.com>
2020-02-26 16:31:27 +0000

    Remove old pre-processing code

--- src/preprocessing/.ipynb_checkpoints/helpers-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def save_data(X, y, col_names, filename):
-    # Save data to file
-    data = pd.DataFrame(data=X, columns=col_names[0:len(col_names) - 1])
-    data['target'] = y
-    data.to_csv(filename, index=False)
